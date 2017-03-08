210451cc4bd958337ca53efd6a2541b5c3967436
taekyeom.oh <taekyeom.oh@nhnent.com>
2017-03-08 17:38:06 +0900

    rebuilding site 2017년 3월  8일 수요일 17시 38분 06초 KST

--- scripts/publish_toghpages.sh
+++ scripts/publish_toghpages.sh
@@ -4,6 +4,19 @@ DIR=$(dirname "$0")
 
 cd $DIR/..
 
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+
 if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
